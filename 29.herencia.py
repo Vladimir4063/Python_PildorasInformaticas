645efc3a59ce922ec130fed0ca72954e2c3a2684
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-03-08 16:55:06 -0300

    Clase/Video 30 - Herencia Multiple

--- 29.herencia.py
+++ 29.herencia.py
@@ -17,14 +17,58 @@ class Vehiculos():
         self.frena=True
 
     def estado(self):
-        print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn Marcha: ", self.enMarcha, "\nAcelerando: ", self.acelera, "\nFrenando: ", self.frena)
+        print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn Marcha: ", 
+            self.enMarcha, "\nAcelerando: ", self.acelera, "\nFrenando: ", self.frena)
+
+
+class Furgoneta(Vehiculos):
+
+    def carga(self, cargar):
+        self.cargado=cargar
+        if(self.cargado):
+            return "La furgoneta está cargada"
+        else:
+            return "La furgoneta no está cargada"
+
 
 class Moto(Vehiculos): #Si no heredamos de la clase "Vehiculos", daria error.
+    
+    hCaballito = ""
+    def caballito(self):
+        self.hCaballito = "Voy haciendo el caballito"
+
+    def estado(self):
+        print("Marca: ", self.marca, "\nModelo: ", self.modelo, "\nEn Marcha: ",
+              self.enMarcha, "\nAcelerando: ", self.acelera, "\nFrenando: ", self.frena, "\n", self.hCaballito)
+
+class VElectricos():
+
+    def __init__(self):
+
+        self.autonomia=100
+
+    def cargarEnergia(self):
+
+        self.cargando=True
+
+class BicicletaElectrica(VElectricos, Vehiculos): #Herencia multiple.
+
     pass
 
+#Instancias
 
 miMoto=Moto("Honda", "CBR") #Estoy pasando los parametros a la nueva clase
 
+miMoto.caballito()
+
 miMoto.estado() #Imprimo los datos atraves del atributo "estado"
 
-miMoto.arrancar() #Terminar <-- No cambia a "True"
+miFurgoneta=Furgoneta("Renault", "Kangoo")
+
+miFurgoneta.arrancar()
+
+miFurgoneta.estado()
+
+print(miFurgoneta.carga(True))
+
+miBici=BicicletaElectrica()
