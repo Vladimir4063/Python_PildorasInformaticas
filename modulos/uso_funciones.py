fff1b827e8e40fbf4fd42148818f586833a3500c
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-04-05 17:04:13 -0300

    Start modules

---
+++ modulos/uso_funciones.py
@@ -0,0 +1,9 @@
+#import funciones_matematicas
+from funciones_matematicas import restar, sumar, multiplicar # Si ponemos * importamos TODAS las librerias.
+
+
+#funciones_matematicas.restar(40, 10)
+restar(40,10)
+
+sumar(20,20)
+multiplicar(2,30)
