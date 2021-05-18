05a4376f81547d30c95ff00d52dbe328dbff7fc7
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-05-18 20:51:35 -0300

    Empezando con interface Grafica

---
+++ interface_grafica/primer_interface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+raiz=Tk()
+
+raiz.title("Primer ventana")
+
+raiz.resizable(1,0) #De esta forma solo puedo expandir a los lados. Si ambos fueran 0 o False, no se podria agrandar.
+
+
+
+raiz.mainloop()
