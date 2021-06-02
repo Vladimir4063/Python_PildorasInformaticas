cf441a7e308cad49c94a53d648bf06a29a9f1d71
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-02 17:39:43 -0300

    calculadora, logica 1

--- 47.calculadora/calculator.py
+++ 47.calculadora/calculator.py
@@ -7,18 +7,53 @@ root.geometry("250x250")
 miFrame= Frame(root)
 miFrame.pack()
 
+operacion =" "
+resultado = 0
+
+#-------- pulsaciones teclado ------#
+
+numeroPantalla = StringVar()
+
+def numeroPulsado(num):
+
+    global operacion
+
+    if operacion!=" ": # Si el usuario opimio +, solo mostrame por pantalla el valor"
+        
+        numeroPantalla.set(num) 
+        operacion= " "
+    
+    else:
+        numeroPantalla.set(numeroPantalla.get() + num)
+
+    
+#------------- sumar ---------------#
+
+def suma(num):
+
+    global operacion
+    global resultado
+
+    resultado+=int(num)
+
+    operacion= "suma"
+
+    numeroPantalla.set(resultado)
+    
+
+
 #------------ pantalla -------------#
 
-pantalla=Entry(miFrame)
+pantalla=Entry(miFrame, textvariable=numeroPantalla)
 pantalla.grid(row=1, column=1, padx= 10, pady=10, columnspan= 4)
-pantalla.config(bg= "black", fg= "green", justify="right")
+pantalla.config(bg= "grey", fg= "white", justify="right")
 
 #------------ fila 1 -------------#
 
-bt7=Button(miFrame, text= "7", width=3)
-bt8=Button(miFrame, text= "8", width=3)
-bt9=Button(miFrame, text= "9", width=3)
-btDiv=Button(miFrame, text= "/", width=3)
+bt7=Button(miFrame, text= "7", width=3, command=lambda:numeroPulsado("7"))
+bt8=Button(miFrame, text= "8", width=3, command=lambda:numeroPulsado("8"))
+bt9=Button(miFrame, text= "9", width=3, command=lambda:numeroPulsado("9"))
+btDiv=Button(miFrame, text= "/", width=3, command=lambda:numeroPulsado("0"))
 
 bt7.grid(row=2, column=1)
 bt8.grid(row=2, column=2)
@@ -27,10 +62,10 @@ btDiv.grid(row=2, column=4)
 
 #------------ fila 2 -------------#
 
-bt4 = Button(miFrame, text="4", width=3)
-bt5 = Button(miFrame, text="5", width=3)
-bt6 = Button(miFrame, text="6", width=3)
-btMult = Button(miFrame, text="x", width=3)
+bt4 = Button(miFrame, text="4", width=3, command=lambda:numeroPulsado("4"))
+bt5 = Button(miFrame, text="5", width=3, command=lambda:numeroPulsado("5"))
+bt6 = Button(miFrame, text="6", width=3, command=lambda:numeroPulsado("6"))
+btMult = Button(miFrame, text="x", width=3, command=lambda:numeroPulsado("0"))
 
 bt4.grid(row=3, column=1)
 bt5.grid(row=3, column=2)
@@ -39,10 +74,10 @@ btMult.grid(row=3, column=4)
 
 #------------ fila 3 -------------#
 
-bt1 = Button(miFrame, text="1", width=3)
-bt2 = Button(miFrame, text="2", width=3)
-bt3 = Button(miFrame, text="3", width=3)
-btRest = Button(miFrame, text="-", width=3)
+bt1 = Button(miFrame, text="1", width=3, command=lambda: numeroPulsado("1"))
+bt2 = Button(miFrame, text="2", width=3, command=lambda:numeroPulsado("2"))
+bt3 = Button(miFrame, text="3", width=3, command=lambda:numeroPulsado("3"))
+btRest = Button(miFrame, text="-", width=3, command=lambda: numeroPulsado("0"))
 
 bt1.grid(row=4, column=1)
 bt2.grid(row=4, column=2)
@@ -51,10 +86,10 @@ btRest.grid(row=4, column=4)
 
 #------------ fila 4 -------------#
 
-bt0 = Button(miFrame, text="0", width=3)
-btComa = Button(miFrame, text=",", width=3)
-btResul = Button(miFrame, text="=", width=3)
-btSum = Button(miFrame, text="+", width=3)
+bt0 = Button(miFrame, text="0", width=3, command=lambda:numeroPulsado("0"))
+btComa = Button(miFrame, text=",", width=3, command=lambda: numeroPulsado("."))
+btResul = Button(miFrame, text="=", width=3, command=lambda:numeroPulsado("0"))
+btSum = Button(miFrame, text="+", width=3, command=lambda: suma(numeroPantalla.get()))
 
 bt0.grid(row=5, column=1)
 btComa.grid(row=5, column=2)
