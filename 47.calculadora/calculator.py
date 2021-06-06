f8a6d3b553fbff91a2fc35d3adfbdedbf14529fa
Vladimir Gutierrez <vladimirgutierrez4063@gmail.com>
2021-06-06 12:45:20 -0300

    func multi en proceso

--- 47.calculadora/calculator.py
+++ 47.calculadora/calculator.py
@@ -9,6 +9,7 @@ miFrame.pack()
 
 operacion =" "
 resultado = 0
+reset_pantalla = False
 
 #-------- pulsaciones teclado ------#
 
@@ -30,9 +31,11 @@ def numeroPulsado(num):
 
 def el_resultado():
     global resultado
-    
-    numeroPantalla.set(resultado + int(numeroPantalla.get()))
-    resultado = 0
+    global operacion
+
+    if operacion == "suma":    
+        numeroPantalla.set(resultado + int(numeroPantalla.get()))
+        resultado = 0
 
 #------------- sumar ---------------#
 
@@ -40,31 +43,70 @@ def suma(num):
 
     global operacion
     global resultado
+    global reset_pantalla
 
     resultado+=int(num)
     operacion= "suma"
+
+    reset_pantalla = True
+
     numeroPantalla.set(resultado)
 
 #------------- restar ---------------#
-#NO TERMINADO
+
+num1 = 0
+contador_resta = 0
 def resta(num):
     global operacion
     global resultado
+    global num1
+    global contador_resta
+    global reset_pantalla
 
-    resultado = resultado - int(num)
+    if contador_resta == 0:
+        num1 = int(num)
+        resultado = num1
+    else: 
+        if contador_resta == 1:
+            resultado = num1 - int(num)
+        else:
+            resultado = int(resultado) - int(num)
+        
+        numeroPantalla.set(resultado)
+        resultado = numeroPantalla.get()
+
+    contador_resta = contador_resta + 1
     operacion = "resta"
-    numeroPantalla.set(resultado)
+    reset_pantalla = True
 
 #------------- multi ---------------#
-#NO TERMINADO
+contador_multi = 0
 def multi(num):
     global operacion
     global resultado
+    global contador_multi
+    global num1
+    global reset_pantalla
+    """
+    if contador_multi == 0:
+        num1 = int(num)
+        resultado = num1
+    else:
+        if contador_multi == 1:
+            resultado = num1*int(num)
+        else:
+            resultado = int(resultado) * int(num)
+        
+        numeroPantalla.set(resultado)
+        resultado=numeroPantalla.get()
+    """
 
+    
     resultado1 = int(num)
     operacion = "multi"
-    resultado = int(numeroPantalla.get()) * resultado1
-
+    num1 = int(numeroPantalla.get()) * resultado1 #Captura el valor de pantalla, y multi con el resul1
+    resultado = num1 * resultado1
+    
     numeroPantalla.set(resultado) #Envio resultado a pantalla
 
 #------------- limpiar pantalla ---------------#
@@ -73,11 +115,11 @@ def limpiarPantalla():
     pantalla.delete(0, END)
 
 def borrar():
-    display_state = pantalla.get()
-    if len(display_state):
-        display_new_state = display_state[:-1]
+    estado_pantalla = pantalla.get()
+    if len(estado_pantalla):
+        nuevoEstado_Pantala = estado_pantalla[:-1]
         limpiarPantalla()
-        pantalla.insert(0, display_new_state)
+        pantalla.insert(0, nuevoEstado_Pantala)
     else:
         limpiarPantalla()
         pantalla.insert(0, 'Error')
@@ -89,7 +131,7 @@ pantalla.grid(row=1, column=1, padx= 10, pady=10, columnspan= 4)
 pantalla.config(bg= "grey", fg= "white", justify="right")
 
 
-#------------ fila 0 -------------#
+#------------ fila 1 -------------#
 
 btCE = Button(miFrame, text= "CE", width=3, command=lambda:limpiarPantalla())
 btC = Button(miFrame, text= "C", width=3, command=lambda:borrar())
@@ -99,7 +141,7 @@ btCE.grid(row=2, column=1)
 btC.grid(row=2, column=2)
 btDelete.grid(row=2, column=3, columnspan=2, sticky=W+E)
 
-#------------ fila 1 -------------#
+#------------ fila 2 -------------#
 
 bt7=Button(miFrame, text= "7", width=3, command=lambda:numeroPulsado("7"))
 bt8=Button(miFrame, text= "8", width=3, command=lambda:numeroPulsado("8"))
@@ -111,7 +153,7 @@ bt8.grid(row=3, column=2)
 bt9.grid(row=3, column=3)
 btDiv.grid(row=3, column=4)
 
-#------------ fila 2 -------------#
+#------------ fila 3 -------------#
 
 bt4 = Button(miFrame, text="4", width=3, command=lambda:numeroPulsado("4"))
 bt5 = Button(miFrame, text="5", width=3, command=lambda:numeroPulsado("5"))
@@ -123,7 +165,7 @@ bt5.grid(row=4, column=2)
 bt6.grid(row=4, column=3)
 btMult.grid(row=4, column=4)
 
-#------------ fila 3 -------------#
+#------------ fila 4 -------------#
 
 bt1 = Button(miFrame, text="1", width=3, command=lambda: numeroPulsado("1"))
 bt2 = Button(miFrame, text="2", width=3, command=lambda:numeroPulsado("2"))
@@ -135,7 +177,7 @@ bt2.grid(row=5, column=2)
 bt3.grid(row=5, column=3)
 btRest.grid(row=5, column=4)
 
-#------------ fila 4 -------------#
+#------------ fila 5 -------------#
 
 bt0 = Button(miFrame, text="0", width=3, command=lambda:numeroPulsado("0"))
 btComa = Button(miFrame, text=",", width=3, command=lambda: numeroPulsado("."))
